e2d4478e68ad54b7958af2779188365473ff1643
Geoff Garbers <geoff@garbers.co.za>
2021-05-10 13:49:03 +0200

    Move instance template to separate file. Add update_policy on zonal instance group manager.

--- main.tf
+++ main.tf
@@ -1,41 +1,3 @@
-resource google_compute_instance_template template {
-  name_prefix = "${var.name}-"
-  machine_type = var.machine_type
-  labels = var.labels
-
-  dynamic service_account {
-    for_each = var.service_account_email != null ? [var.service_account_email] : []
-    content {
-      scopes = ["cloud-platform"]
-      email = service_account.value
-    }
-  }
-
-  disk {
-    disk_size_gb = var.disk_size
-    disk_type = var.disk_type
-    source_image = "cos-cloud/cos-stable"
-    auto_delete = true
-  }
-
-  network_interface {
-    network = var.network
-  }
-
-  metadata = {
-    user-data = "#cloud-config\n${yamlencode(local.cloudinit_config)}"
-  }
-
-  scheduling {
-    preemptible = var.preemptible
-    automatic_restart = !var.preemptible
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
 resource google_compute_region_instance_group_manager manager {
   count = local.is_regional_manager ? 1 : 0
 
@@ -63,6 +25,11 @@ resource google_compute_instance_group_manager manager {
   zone = var.location
   target_size = var.instance_count
   wait_for_instances = true
+
+  update_policy {
+    minimal_action = "REPLACE"
+    type = "PROACTIVE"
+  }
 
   version {
     instance_template = google_compute_instance_template.template.self_link
