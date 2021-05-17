ebd37a01ba806b4fffffae5a36ddf6196ad35e7d
Geoff Garbers <geoff@garbers.co.za>
2021-05-17 11:40:45 +0200

    Update README, and add missing outputs.

--- outputs.tf
+++ outputs.tf
@@ -50,6 +50,18 @@ output cloudsql_path {
   value = var.cloudsql_path
 }
 
+output cloudsql_restart_interval {
+  value = var.cloudsql_restart_interval
+}
+
+output cloudsql_restart_policy {
+  value = var.cloudsql_restart_policy
+}
+
+output cloudsql_wait_duration {
+  value = var.cloudsql_wait_duration
+}
+
 output disk_size {
   value = var.disk_size
 }
@@ -60,6 +72,34 @@ output disk_type {
 
 output env {
   value = var.env
+}
+
+output health_check_enabled {
+  value = var.health_check_enabled
+}
+
+output health_check_port {
+  value = var.health_check_port
+}
+
+output health_check_name {
+  value = var.health_check_name
+}
+
+output health_check_interval {
+  value = var.health_check_interval
+}
+
+output health_check_healthy_threshold {
+  value = var.health_check_healthy_threshold
+}
+
+output health_check_initial_delay {
+  value = var.health_check_initial_delay
+}
+
+output health_check_unhealthy_threshold {
+  value = var.health_check_unhealthy_threshold
 }
 
 output instance_count {
@@ -90,6 +130,18 @@ output preemptible {
   value = var.preemptible
 }
 
+output restart_interval {
+  value = var.restart_interval
+}
+
+output restart_policy {
+  value = var.restart_policy
+}
+
+output runcmd {
+  value = var.runcmd
+}
+
 output service_account_email {
   value = var.service_account_email
 }
@@ -100,4 +152,12 @@ output systemd_name {
 
 output timers {
   value = var.timers
+}
+
+output timezone {
+  value = var.timezone
+}
+
+output wait_for_instances {
+  value = var.wait_for_instances
 }
