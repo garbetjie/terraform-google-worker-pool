0c596640b421baf5af7b281cde5834be0941014e
Geoff Garbers <geoff@garbers.co.za>
2021-06-02 21:31:22 +0200

    consolidate cloudsql config to single input

--- cloudsql.tf
+++ cloudsql.tf
@@ -1,48 +1,35 @@
-variable cloudsql_connections {
-  type = set(string)
-  default = []
-  description = "List of CloudSQL connections to establish before starting workers."
-}
+variable cloudsql {
+  type = object({
+    connections = set(string)
+    wait_duration = optional(number)
+    mount_name = optional(string)
+    mount_path = optional(string)
+    restart_policy = optional(string)  // ["no", "on-success", "on-failure", "on-abnormal", "on-watchdog", "on-abort", "always"]
+    restart_interval = optional(number)
+  })
 
-variable cloudsql_mount_path {
-  type = string
-  default = "/cloudsql"
-  description = "The path at which CloudSQL connection sockets will be available in workers and timers."
-}
-
-variable cloudsql_restart_interval {
-  type = number
-  default = 5
-  description = "Number of seconds to wait before restarting the CloudSQL service if it stops."
-}
-
-variable cloudsql_restart_policy {
-  type = string
-  default = "always"
-  description = "The restart policy to apply to the CloudSQL service."
-
-  validation {
-    condition = contains(["no", "on-success", "on-failure", "on-abnormal", "on-watchdog", "on-abort", "always"], var.cloudsql_restart_policy)
-    error_message = "CloudSQL restart policy must be one of [always, no, on-success, on-failure, on-abnormal, on-watchdog, on-abort]."
+  default = {
+    connections = []
+    wait_duration = 30
+    mount_name = "cloudsql"
+    mount_path = "/cloudsql"
+    restart_policy = "always"
+    restart_interval = 5
   }
 }
 
-variable cloudsql_wait_duration {
-  type = number
-  default = 30
-  description = "How long to wait (in seconds) for CloudSQL connections to be established before starting workers."
-}
-
 locals {
-  cloudsql_connections = var.cloudsql_connections
-  cloudsql_mount_name = "cloudsql"
-  cloudsql_mount_path = var.cloudsql_mount_path
-  cloudsql_restart_policy = var.cloudsql_restart_policy
-  cloudsql_restart_interval = var.cloudsql_restart_interval
-  cloudsql_wait_duration = var.cloudsql_wait_duration
+  cloudsql = {
+    connections = var.cloudsql.connections
+    wait_duration = var.cloudsql.wait_duration == null ? 30 : var.cloudsql.wait_duration
+    mount_name = coalesce(var.cloudsql.mount_name, "cloudsql")
+    mount_path = coalesce(var.cloudsql.mount_path, "/cloudsql")
+    restart_policy = coalesce(var.cloudsql.restart_policy, "always")
+    restart_interval = coalesce(var.cloudsql.restart_interval, 5)
+  }
 
-  cloudsql_required = length(local.cloudsql_connections) > 0
-  cloudsql_wait = local.cloudsql_required && local.cloudsql_wait_duration >= 0
+  cloudsql_required = length(local.cloudsql.connections) > 0
+  cloudsql_wait = local.cloudsql_required && local.cloudsql.wait_duration >= 0
 
   cloudsql_systemd_exec_start_pre = local.cloudsql_wait ? ["/bin/sh /etc/runtime/scripts/wait-for-cloudsql.sh"] : []
   cloudsql_systemd_requires = local.cloudsql_required ? ["cloudsql.service"] : []
@@ -51,8 +38,8 @@ locals {
     jsondecode(
       templatefile("${path.module}/templates/mount.json.tpl", {
         type = "volume",
-        src = local.cloudsql_mount_name,
-        target = local.cloudsql_mount_path,
+        src = local.cloudsql.mount_name,
+        target = local.cloudsql.mount_path,
         readonly = true
       })
     )
@@ -61,39 +48,30 @@ locals {
   // Unit files to be included in the cloud-init config.
   unit_files_cloudsql = local.cloudsql_required ? {
     "cloudsql.service" = templatefile("${path.module}/templates/systemd-cloudsql.tpl", {
-      connections = local.cloudsql_connections
-      mount_name = local.cloudsql_mount_name
-      restart = local.cloudsql_restart_policy
-      restart_sec = local.cloudsql_restart_interval
+      connections = local.cloudsql.connections
+      mount_name = local.cloudsql.mount_name
+      restart = local.cloudsql.restart_policy
+      restart_sec = local.cloudsql.restart_interval
     })
   } : {}
 
   // Script files to be included in the cloud-init config.
   script_files_cloudsql = local.cloudsql_wait ? {
     "wait-for-cloudsql.sh" = templatefile("${path.module}/templates/script-wait-for-cloudsql.sh.tpl", {
-      wait_duration = local.cloudsql_wait_duration
+      wait_duration = local.cloudsql.wait_duration
     })
   } : {}
 }
 
-output cloudsql_connections {
-  value = local.cloudsql_connections
-}
-
-output cloudsql_mount_path {
-  value = local.cloudsql_mount_path
-}
-
-output cloudsql_restart_policy {
-  value = local.cloudsql_restart_policy
-}
-
-output cloudsql_restart_interval {
-  value = local.cloudsql_restart_interval
-}
-
-output cloudsql_wait_duration {
-  value = local.cloudsql_wait_duration
+output cloudsql {
+  value = {
+    connections = local.cloudsql.connections
+    wait_duration = local.cloudsql.wait_duration
+    mount_name = local.cloudsql.mount_name
+    mount_path = local.cloudsql.mount_path
+    restart_policy = local.cloudsql.restart_policy
+    restart_interval = local.cloudsql.restart_interval
+  }
 }
 
 output cloudsql_wait {
